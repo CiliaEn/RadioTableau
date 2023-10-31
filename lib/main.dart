0f9ad4a949177631d6d7681e938f7ba0d1d37521
ciliaence <cilia.ence@telia.com>
2023-10-31 14:18:58 +0100

    fetch data fr api

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:xml/xml.dart';
 
 //first comment
 //Cilia Ence 31 october 2023
@@ -10,31 +12,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: ''),
     );
   }
 }
@@ -42,87 +28,128 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ScheduleEpisode {
+  final String title;
+  final String description;
+  final String startTimeUtc;
+  final String endTimeUtc;
+  final String? programId;
+  final String? programName;
+  final String? channelId;
+  final String? channelName;
+  final String imageUrl;
 
-  void _incrementCounter() {
+  ScheduleEpisode({
+    required this.title,
+    required this.description,
+    required this.startTimeUtc,
+    required this.endTimeUtc,
+    required this.programId,
+    required this.programName,
+    required this.channelId,
+    required this.channelName,
+    required this.imageUrl,
+  });
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<ScheduleEpisode> _scheduleEpisodes = [];
+
+  final dio = Dio();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  String _firstProgramName = 'no sé';
+
+  void _fetchData() async {
+    const channelID = '164'; // Set your channel ID here
+
+    Response response = await dio
+        .get('https://api.sr.se/v2/scheduledepisodes?channelid=$channelID');
+
+    final document = XmlDocument.parse(response.data);
+
+    final scheduleElements = document.findAllElements('scheduledepisode');
+
+    List<ScheduleEpisode> scheduleEpisodes = scheduleElements.map((element) {
+      final title = element.findElements('title').single.text;
+      final description = element.findElements('description').single.text;
+      final startTimeUtc = element.findElements('starttimeutc').single.text;
+      final endTimeUtc = element.findElements('endtimeutc').single.text;
+      final programId =
+          element.findElements('program').first.getAttribute('id');
+      final programName =
+          element.findElements('program').first.getAttribute('name');
+      final channelId =
+          element.findElements('channel').first.getAttribute('id');
+      final channelName =
+          element.findElements('channel').first.getAttribute('name');
+      final imageUrl = element.findElements('imageurl').single.text;
+
+      return ScheduleEpisode(
+        title: title,
+        description: description,
+        startTimeUtc: startTimeUtc,
+        endTimeUtc: endTimeUtc,
+        programId: programId,
+        programName: programName,
+        channelId: channelId,
+        channelName: channelName,
+        imageUrl: imageUrl,
+      );
+    }).toList();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _scheduleEpisodes = scheduleEpisodes;
     });
+
+    if (_scheduleEpisodes.isNotEmpty) {
+      setState(() {
+        _firstProgramName = _scheduleEpisodes[0].title;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme.of(context).colorScheme.background,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              "Tablå för P1",
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _scheduleEpisodes.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_scheduleEpisodes[index].title),
+                    subtitle: Text(_scheduleEpisodes[index].description),
+                    // You can display more information here as needed
+                  );
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
