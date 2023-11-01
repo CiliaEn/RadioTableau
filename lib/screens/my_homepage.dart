2a4c88f190827e61a55347c003c88f47423c9130
ciliaence <cilia.ence@telia.com>
2023-11-01 15:23:17 +0100

    first draft of design

--- lib/screens/my_homepage.dart
+++ lib/screens/my_homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:radiotableau/models/schedule_episode.dart';
 import 'package:radiotableau/services/api_service.dart';
+import 'package:radiotableau/widgets/episode_list_item.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -32,30 +33,53 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Tablå för P1",
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/assets/Background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Tablå för P1',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    'Idag',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _scheduleEpisodes.length,
+                    itemBuilder: (context, index) {
+                      return EpisodeListItem(
+                        episode: _scheduleEpisodes[index],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _scheduleEpisodes.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_scheduleEpisodes[index].title),
-                    subtitle: Text(_scheduleEpisodes[index].description),
-                  );
-                },
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:radiotableau/models/schedule_episode.dart';
 import 'package:radiotableau/utils/constants.dart';
 import 'package:xml/xml.dart';
+import 'package:intl/intl.dart';
 
 class ApiService {
   final dio = Dio();
@@ -9,42 +10,60 @@ class ApiService {
   Future<List<ScheduleEpisode>> fetchData() async {
     const String channelID = Constants.channelID;
 
-    Response response = await dio
-        .get('https://api.sr.se/v2/scheduledepisodes?channelid=$channelID');
+    try {
+      Response response = await dio
+          .get('https://api.sr.se/v2/scheduledepisodes?channelid=$channelID');
 
-    final document = XmlDocument.parse(response.data);
+      final document = XmlDocument.parse(response.data);
 
-    final scheduleElements = document.findAllElements('scheduledepisode');
+      final scheduleElements = document.findAllElements('scheduledepisode');
 
-    List<ScheduleEpisode> scheduleEpisodes = scheduleElements.map((element) {
-      final title = element.findElements('title').single.innerText;
-      final description = element.findElements('description').single.innerText;
-      final startTimeUtc =
-          element.findElements('starttimeutc').single.innerText;
-      final endTimeUtc = element.findElements('endtimeutc').single.innerText;
-      final programId =
-          element.findElements('program').first.getAttribute('id');
-      final programName =
-          element.findElements('program').first.getAttribute('name');
-      final channelId =
-          element.findElements('channel').first.getAttribute('id');
-      final channelName =
-          element.findElements('channel').first.getAttribute('name');
-      final imageUrl = element.findElements('imageurl').single.innerText;
+      List<ScheduleEpisode> scheduleEpisodes = scheduleElements.map((element) {
+        final title = element.findElements('title').isNotEmpty
+            ? element.findElements('title').single.innerText
+            : '';
+        final description = element.findElements('description').isNotEmpty
+            ? element.findElements('description').single.innerText
+            : '';
+        final startTimeUtc = element.findElements('starttimeutc').isNotEmpty
+            ? element.findElements('starttimeutc').single.innerText
+            : '';
+        final endTimeUtc = element.findElements('endtimeutc').isNotEmpty
+            ? element.findElements('endtimeutc').single.innerText
+            : '';
+        final programId = element.findElements('program').isNotEmpty
+            ? element.findElements('program').first.getAttribute('id')
+            : '';
+        final programName = element.findElements('program').isNotEmpty
+            ? element.findElements('program').first.getAttribute('name')
+            : '';
+        final channelId = element.findElements('channel').isNotEmpty
+            ? element.findElements('channel').first.getAttribute('id')
+            : '';
+        final channelName = element.findElements('channel').isNotEmpty
+            ? element.findElements('channel').first.getAttribute('name')
+            : '';
+        final imageUrl = element.findElements('imageurl').isNotEmpty
+            ? element.findElements('imageurl').single.innerText
+            : '';
 
-      return ScheduleEpisode(
-        title: title,
-        description: description,
-        startTimeUtc: startTimeUtc,
-        endTimeUtc: endTimeUtc,
-        programId: programId,
-        programName: programName,
-        channelId: channelId,
-        channelName: channelName,
-        imageUrl: imageUrl,
-      );
-    }).toList();
+        return ScheduleEpisode(
+          title: title,
+          description: description,
+          startTimeUtc: startTimeUtc,
+          endTimeUtc: endTimeUtc,
+          programId: programId,
+          programName: programName,
+          channelId: channelId,
+          channelName: channelName,
+          imageUrl: imageUrl,
+        );
+      }).toList();
 
-    return scheduleEpisodes;
+      return scheduleEpisodes;
+    } catch (e) {
+      print('Error fetching data: $e');
+      return [];
+    }
   }
 }
